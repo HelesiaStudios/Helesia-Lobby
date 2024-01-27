4c73ee4aa1d38f37884921687e485dd708b79d36
6myy <155362207+6myy@users.noreply.github.com>
2024-01-27 16:12:35 +0100

    Update

--- src/main/java/fr/helesia/lobby/Main.java
+++ src/main/java/fr/helesia/lobby/Main.java
@@ -2,13 +2,11 @@ package fr.helesia.lobby;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import fr.helesia.lobby.listener.player.PlayerListener;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
 
     }
 
