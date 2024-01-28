6fbd712bbc0af2bfe3bed8df97f7ccf794534050
6myy <155362207+6myy@users.noreply.github.com>
2024-01-28 20:04:48 +0100

    Add scoreboard and api mongodb

--- src/main/java/fr/helesia/lobby/Main.java
+++ src/main/java/fr/helesia/lobby/Main.java
@@ -1,18 +1,48 @@
 package fr.helesia.lobby;
 
 import fr.helesia.lobby.listener.player.PlayerListener;
+import fr.helesia.lobby.scoreboard.ScoreboardManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 public final class Main extends JavaPlugin {
+    private static Main instance;
+    private ScoreboardManager scoreboardManager;
+
+    private ScheduledExecutorService executorMonoThread;
+    private ScheduledExecutorService scheduledExecutorService;
 
     @Override
     public void onEnable() {
+        instance = this;
         Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+
+        scheduledExecutorService = Executors.newScheduledThreadPool(16);
+        executorMonoThread = Executors.newScheduledThreadPool(1);
+        scoreboardManager = new ScoreboardManager();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getScoreboardManager().onDisable();
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public ScoreboardManager getScoreboardManager() {
+        return scoreboardManager;
+    }
+
+    public ScheduledExecutorService getExecutorMonoThread() {
+        return executorMonoThread;
+    }
+
+    public ScheduledExecutorService getScheduledExecutorService() {
+        return scheduledExecutorService;
     }
 }
